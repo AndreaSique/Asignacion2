d3eeb521531cb100812f023f6ac399f358f657a9
AndreaSique <69050106+AndreaSique@users.noreply.github.com>
2021-09-14 10:35:05 -0600

    se modificó la programación

--- js/Main.js
+++ js/Main.js
@@ -2,16 +2,9 @@ function calcularIMC(elEvento){
     elEvento.preventDefault()
 
 var nombre = document.querySelector("#inputPrimerNombre").value;
-var sexoMasculino = document.querySelector("#input_hombre").value;
-var sexoFemenino = document.querySelector("#input_mujer").value;
 var peso = document.querySelector("#inputPeso").value;
 var altura = document.querySelector("#inputAltura").value;
-var edad = document.querySelector("#inputEdad").value;
-var sedentario = document.querySelector("#sedentario");
-var moderado = document.querySelector("#moderado");
-var ligero = document.querySelector("#ligero");
-var intenso = document.querySelector("#intenso");
-var medidaAbdominal = document.querySelector("#inputMedidaAbdominal").value;
+
 
 /* Section formula */
 var formula = peso/(altura*altura);
@@ -19,13 +12,13 @@ var formula = peso/(altura*altura);
 
 /* Section programacion */
 if(formula < 18.5){
-    cajaMensaje.innerHTML = '<p class="mensaje">IMC: Bajo peso</p>'
+    cajaMensaje.innerHTML = '<p class="mensaje"> '+ nombre +' tienes un bajo peso</p>'
 }else if(formula > 18.5 & formula < 24.9){
-    cajaMensaje.innerHTML = '<p class="mensaje">IMC: Peso normal</p>'
+    cajaMensaje.innerHTML = '<p class="mensaje"> '+ nombre +' tienes un peso normal</p>'
 }else if(formula > 25 & formula < 29.9){
-    cajaMensaje.innerHTML = '<p class="mensaje">IMC: Sobrepeso</p>'
+    cajaMensaje.innerHTML = '<p class="mensaje"> '+ nombre +' tienes sobrepeso</p>'
 }else if(formula >= 30){
-    cajaMensaje.innerHTML = '<p class="mensaje">IMC: Obesidad</p>'
+    cajaMensaje.innerHTML = '<p class="mensaje"> '+ nombre +' tienes obesidad</p>'
 }
 
 
